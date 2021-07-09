9899e9c2f041b12b52f8d965c99ae031ce69a7d6
0tiz <timotee0510@gmail.com>
2021-07-09 12:00:08 +0200

    Blubb

--- irda.py
+++ irda.py
@@ -3,13 +3,12 @@ import serial
 from serial.serialutil import STOPBITS_ONE, Timeout
 from serial.win32 import WaitCommEvent
 import time
-import screen300
 
 com_Port = "COM3"
 
 ser = serial.Serial(port='COM3', baudrate=115000, bytesize=serial.EIGHTBITS,timeout=0,
     parity=serial.PARITY_NONE)
-#ser.flushInput()
+ser.flushInput()
 #ser = serial.Serial(port='COM3', baudrate=115000, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=1)
     
 
@@ -30,41 +29,45 @@ antwortEinschaltsignal = b"\x1bIJ"
 
 
 handshake = False
-einschaltCheck= True
+einschaltCheck= False
 connectTopc = False
 mtkModus = False
 readStream = False
 
-screenRecorder = screen300.Screen300()
+
 #Handshake 
 
 while einschaltCheck == False:
     read = ser.read_all()
-    ser.write(screenRecorder.doHandshake1)
-
-    if screenRecorder.getEingeschaltet in read:
+    ser.write(befehlHandshake1)
+    if antwortEingeschaltet in read:
         print(read)
         print("Screen Recorder wurde eingeschaltet")
-        einschaltCheck = True
-
+        einschaltCheck == True
+    else:
+        print("Schalte den Screen Recorder ein")
     time.sleep(0.6)
 
 while handshake == False:
     read = ser.read_all()
-    ser.write(screenRecorder.doHandshake2)
+    ser.write(befehlHandshake2)
     print(read)
     if antwortFirmware in read:
         print("Handshake erfolgreich")
-        handshake = True
-        
+        handshake == True
     time.sleep(0.6)
 
+while mtkModus == False:
+    read = ser.read_all()
+    ser.write(befehl_MTK_modus)
+    if "SerialNumber:" in read:
+        mtkModus == True
+    time.sleep(0.6)
 
 while readStream == False:
     read = ser.read_all()
-    ser.write(screenRecorder.do_MTK_modus)
     print(read)
-    time.sleep(0.3)
+
     
 
 
